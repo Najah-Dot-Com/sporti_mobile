724d195c82a50ebe0fe8cf910e80a12e7986d037
osama <om107547@gmail.com>
2022-04-04 05:43:13 +0300

    add network layer [use case , feature] make login api

--- lib/feature/model/user_data.dart
+++ lib/feature/model/user_data.dart
@@ -3,21 +3,29 @@ class UserData {
     this.fullname,
     this.token,
     this.expiresIn,
+    this.isVerify = false,
+    this.username
   });
 
   String? fullname;
   String? token;
   DateTime? expiresIn;
+  bool? isVerify;
+  String? username;
 
   factory UserData.fromJson(Map<String, dynamic> json) => UserData(
         fullname:json["fullname"] == null ? null: json["fullname"],
         token:json["token"] == null ? null: json["token"],
+        isVerify:json["isVerify"] == null ? false: json["isVerify"],
+        username:json["username"] == null ? null: json["username"],
         expiresIn:json["expires_in"] == null ? null: DateTime.parse(json["expires_in"]),
       );
 
   Map<String, dynamic> toJson() => {
         "fullname":fullname == null ? null: fullname,
         "token":token == null ? null: token,
+        "isVerify":isVerify == null ? false: isVerify,
+        "username":username == null ? null: username,
         "expires_in":expiresIn == null ? null: expiresIn?.toIso8601String(),
       };
 }
